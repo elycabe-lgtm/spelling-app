394efe05b3ee883dfa06f592e31c18443bb1fae5
elycabe-lgtm <elycabe@gmail.com>
2026-04-19 17:31:36 -0600

    Add files via upload

---
+++ manifest.json.js
@@ -0,0 +1,20 @@
+{
+  "name": "Spelling Adventure",
+  "short_name": "Spelling",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#4facfe",
+  "theme_color": "#4facfe",
+  "icons": [
+    {
+      "src": "assets/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "assets/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
