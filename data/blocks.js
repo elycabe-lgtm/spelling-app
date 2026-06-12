3f1552bb87e25c91ed3cee7e21c8c44cb3df2132
elycabe-lgtm <elycabe@gmail.com>
2026-06-11 19:37:07 -0600

    Add files via upload

---
+++ data/blocks.js
@@ -0,0 +1,23 @@
+const blockNames = {
+  "1": "Bloque 1-1st Grade BOOK 1-3",
+  "2": "Bloque 2-1st Grade BOOK 4",
+  "3": "Bloque 3-Regular Words Concurso",
+  "4": "Bloque 4-Challenging Words Concurso",
+  "5": "Bloque 5-5th Grade BOOK 1",
+  "6": "Bloque 6-5th Grade BOOK 2",
+  "7": "Bloque 7-5th Grade BOOK 3",
+  "8": "Bloque 8-5th Grade BOOK 4",
+  "9": "Bloque 9-Preescolar",
+
+  "10": "Bloque 10-1st Grade BOOK 5",
+
+  "11": "Bloque 11-5th Grade BOOK 5",
+
+  // 🔥 NUEVOS 2nd Grade
+
+  "16": "Bloque 16-2nd Grade BOOK 1",
+  "17": "Bloque 17-2nd Grade BOOK 2",
+  "18": "Bloque 18-2nd Grade BOOK 3",
+  "19": "Bloque 19-2nd Grade BOOK 4",
+  "20": "Bloque 20-2nd Grade BOOK 5"
+}
